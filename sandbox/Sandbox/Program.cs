6d88917c694467c5e83f321e60a54cb23aa5ff45
Alizenoch <alihopa055@gmail.com>
2024-05-03 02:01:26 +1000

    Added the first and last name

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name?" );
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your last name is {last}, {first} {last}"); 
     }
 }
