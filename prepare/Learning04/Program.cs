fd69d90fe65e53d653ea751264eb1e9bf30ac773
Alison Hopa <alihopa055@gmail.com>
2024-05-16 09:04:27 +1000

    Commpleted Prepare 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using library_demo;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+    { 
+        // created a simple base Assignment
+        Assignment a1 = new Assignment("Alison Hopa", "Programming");
+        Console.WriteLine(a1.GetSummary());
+
+        // Here I created a new MathAssignment object and set it's values
+        MathAssignment a2 = new MathAssignment("Alison Hopa", "Subtraction", "2.2", "10-5");
+        // And I tested the two methods
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        // Tested my new class
+        WritingAssignment a3 = new WritingAssignment("Alison Hopa", " Programming", "Inheritance");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.WritingInformation()); 
     }
 }
+
+
