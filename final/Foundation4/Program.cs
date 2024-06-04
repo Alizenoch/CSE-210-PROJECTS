474f7597df2fbec5c815e4c9ae74b491892d1160
Alizenoch <alihopa055@mail.com>
2024-06-03 20:39:26 -0700

    Program.cs

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,17 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        var activities = new Activity[]
+        {
+            new Running { _date = new DateTime(2022, 11, 3), _minutes = 30, _distance = 4.8 },
+            new Cycling { _date = new DateTime(2022, 11, 4), _minutes = 45, _speed = 20 },
+            new Swimming { _date = new DateTime(2022, 11, 5), _minutes = 60, _laps = 20 }
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
