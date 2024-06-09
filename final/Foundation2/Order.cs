1ee93ecbbaa261279ba50df507687846c63c884f
Alizenoch <alihopa055@mail.com>
2024-06-08 21:46:33 -0700

    Order.cs

--- final/Foundation2/Order.cs
+++ final/Foundation2/Order.cs
@@ -1,48 +1,105 @@
-class Order
+public class Order
 {
-    private List<Product> _products;
+    // attributes (all are private)...
+    private List<Product> _products = new List<Product>();
     private Customer _customer;
 
-    public Order(Customer customer)
+    private float _subTotal; // store the price without the shipping cost
+    private float _totalPrice; // store the total price (subTotal + shipping cost)
+    private int _USAShippingCost; // $35
+    private int _outShippingCost; // $5
+
+    // constructor to set customer name to Customer constructor.
+    // Also, with this constructor sets customer street, city, state, and country to Customer constructor,
+    // Then Customer constructor sets them to address constructor...
+    public Order(string nameCustomer, string street, string city, string state, string country)
     {
-        
-        _products = new List<Product>();
-        _customer = customer;
+        _customer = new Customer(nameCustomer, street, city, state, country); // instance a new Customer object to get certain Customer attributes and methods
+
+        _USAShippingCost = 5;
+        _outShippingCost = 35;
     }
 
-    public void AddProduct(Product product)
+    // methods...
+
+    // set products in list method to add each product created in Main class
+    public void SetProductInList(Product product)
     {
         _products.Add(product);
     }
 
-    public decimal CalculateTotalPrice()
+    // packing label
+    public void DisplayPackingLabel()
     {
-        decimal totalCost = 0;
-        foreach(var product in _products)
-        {
-            totalCost += product.CalculateTotalCost();
-        }
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("ORDER\n");
 
-        // Add shipping cost based on customers location
-        decimal shippingCost = _customer.LivesInUSA() ? 5 : 35;
-        return totalCost + shippingCost;
+        for (int i = 0; i < _products.Count; i++)
+        {
+            // get product name, id, price, and quantity from each product object
+            string _nameProduct = _products[i].GetNameProduct();
+            int _idProduct = _products[i].GetIDProduct();
+            
+            float _priceProduct = _products[i].GetPriceProduct();
+            int _quantityProduct = _products[i].GetQuantityProduct();
+
+            Console.WriteLine($"({_idProduct}) {_nameProduct} ${_priceProduct} x {_quantityProduct}");
+        }
+        Console.WriteLine("---------------------------------------");
     }
 
-    public string GetPackingLabel()
+    // shipping label
+    public void DisplayShippingLabel()
     {
-        // Create a packing label with product names and ids
-        string packingLabel = "Packing Label:\n";
-        foreach(var product in _products)
-        {
-          packingLabel +=$"{product.CalculateTotalCost():C}-{product.name}(ID:{product.productID})\n";
-        }
-        return packingLabel;
-        
+        string _nameCustomer = _customer.GetNameCustomer(); // get name of customer from customer object created
+        Console.WriteLine($"Customer: {_nameCustomer}\n"); 
+
+        Console.WriteLine("Address:");
+        string _addressCustomer = _customer.GetAddress(); // get address of customer from customer object created
+        Console.WriteLine($"{_addressCustomer}"); 
     }
 
-    public string GetShippingLabel()
+    // total price
+    public void CalculateTotalPrice()
     {
-        // Create a shipping label with customer name and address.
-        return $"Shipping Label:\n {Customer.name}\n{Customer.customerAddress}";
+        // first, calculate subtotal...
+        for (int i = 0; i < _products.Count; i++)
+        {
+            float _price = _products[i].CalculateRealPrice();
+            _subTotal += _price;
+        }
+
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine($"Sub-Total: ${_subTotal}");
+
+        // adding shipping cost to subtotal depending whether customer lives in the USA or not...
+        if (_customer.GetLivingInUSA() == true)
+        {
+            _totalPrice = _subTotal + _USAShippingCost;
+            Console.WriteLine($"Shipping cost: ${_USAShippingCost}");
+        }
+        else
+        {
+            _totalPrice = _subTotal + _outShippingCost;
+            Console.WriteLine($"Shipping cost: ${_outShippingCost}");
+        }
+
+        // display total price...
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine($"Total: ${_totalPrice}");
+        Console.WriteLine("---------------------------------------");
+    }
+
+    // display order
+    public void DisplayOrder()
+    {
+        Console.WriteLine("");
+
+        // call the methods to get the packing label, the shipping label, and the total price...
+        DisplayPackingLabel();
+        DisplayShippingLabel();
+        CalculateTotalPrice();
+
+        Console.WriteLine("");
     }
 }
